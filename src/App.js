8b04393cac62af2c1e7fc3e3d4a1fed170b07207
AdiKulkarni-Dev <aditya_kulkarni1@hotmail.com>
2023-07-23 19:20:50 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount] =useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='App'>
+    <header>
+      <h1>Counter using state</h1>
+    </header>
+
+  </div>
+
   );
 }
 
