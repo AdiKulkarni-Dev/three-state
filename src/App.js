5556644e7774a3d820d42e14d60958c741faba89
AdiKulkarni-Dev <aditya_kulkarni1@hotmail.com>
2023-07-23 20:09:04 +0530

    Final Counter using states

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
@@ -9,7 +8,10 @@ function App() {
     <header>
       <h1>Counter using state</h1>
     </header>
-
+    <h2>Current value of counter {count}</h2>
+    <button onClick={()=>setCount(0)}>Reset</button>
+    <button onClick={()=>(count>10 ? "" : setCount(count+1))}>Up</button>
+    <button onClick={()=>setCount(count-1)}>Down</button>
   </div>
 
   );
